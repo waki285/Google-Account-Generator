2e2f62ba6c448be71a8a1153e04e3b5c8c3212d3
waki285 <suzuneu@suzuneu.com>
2022-01-14 18:48:20 +0900

    commits

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { magenta, bgWhite, green, cyan, yellowBright, red, bgRed } from "chalk";
 import { config } from "dotenv";
 config();
 
-const K = `[${magenta("*")}] ${bgWhite("GAG")}`;
+const K = `[${magenta("*")}] ${bgWhite.black("GAG")}`;
 const SUCCESS = green("SUCCESS!");
 const INFO = cyan("info");
 const WARN = yellowBright("WARNING");
@@ -26,6 +26,26 @@ class Generator extends EventEmitter {
   private _browser: Browser | null;
   constructor(options?: GeneratorOptions) {
     super();
-    this._use2captcha = options.use2captcha
+    if (options?.use2captcha) this._use2captcha = options.use2captcha; else this._use2captcha = true;
+    this._browser = null;
+    console.log(`${K} ${SUCCESS} initialized.`);
+  };
+  async launch(): Promise<Browser> {
+    console.log(`${K} ${INFO} launching browser...`);
+    this._browser = await puppeteer.launch({
+      headless: false,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox"
+      ]
+    });
+    console.log(`${K} ${SUCCESS} browser launched.`);
+    this.emit("browserLaunched");
+    return this._browser
   }
-}
+};
+
+(async () => {
+  const gen = new Generator();
+  await gen.launch()
+})()
