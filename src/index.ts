642f9488c070426edcd5922f0c398611a01a42c8
waki285 <suzuneu@suzuneu.com>
2022-01-15 14:03:25 +0900

    a

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import axios from "axios";
 import stealthPlugin from "puppeteer-extra-plugin-stealth";
 import { EventEmitter } from "events";
 import { magenta, bgWhite, green, cyan, yellowBright, red, bgRed } from "chalk";
-import "../typings/types";
-
+import { name } from "faker";
 import { config } from "dotenv";
 config();
 
@@ -22,8 +21,31 @@ type GeneratorOptions = {
   use2captcha?: boolean
 }
 
+class GAGError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = `${K} ${FATAL}`;
+  };
+}
 
-class Generator extends EventEmitter {
+type GeneratorEvents = "browserLaunched" | "wentToGoogle";
+
+class StrictEventEmitter extends EventEmitter {
+  on(eventName: GeneratorEvents, listener: (...args: any[]) => void): this {
+    return super.on(eventName, listener);
+  };
+  once(eventName: GeneratorEvents, listener: (...args: any[]) => void): this {
+    return super.once(eventName, listener);
+  };
+  emit(eventName: GeneratorEvents, ...args: any[]): boolean {
+    return super.emit(eventName, ...args);
+  };
+  off(eventName: GeneratorEvents, listener: (...args: any[]) => void): this {
+    return super.off(eventName, listener);
+  };
+}
+
+class Generator extends StrictEventEmitter {
   private _use2captcha: boolean
   private _browser: Browser | null;
   constructor(options?: GeneratorOptions) {
@@ -45,9 +67,19 @@ class Generator extends EventEmitter {
     this.emit("browserLaunched");
     return this._browser;
   }
+  async gotoGoogle(): Promise<Page> {
+    if (!this._browser) throw new GAGError("You don't launch browser! please run generator.launch()");
+    console.log(`${K} ${INFO} go to https://google.com register page.`);
+    const page: Page = (await this._browser.pages())[0] || await this._browser.newPage();
+    await page.goto("https://accounts.google.com/signup/v2/webcreateaccount?continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dgoogle%26rlz%3D1C1FQRR_jaJP983JP983%26oq%3Dgoogle%26aqs%3Dchrome..69i57j0i271l3.579j0j1%26sourceid%3Dchrome%26ie%3DUTF-8&hl=ja&dsh=S-1139929386%3A1642222684357616&biz=false&flowName=GlifWebSignIn&flowEntry=SignUp")
+    console.log(`${K} ${SUCCESS} success go to google register page.`);
+    this.emit("wentToGoogle");
+    return page;
+  }
 };
 
 (async () => {
   const gen = new Generator();
   await gen.launch()
+  await gen.gotoGoogle();
 })()
